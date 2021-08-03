08774cd009eefa2572c05bbf49bde6154c1a569b
a0975844268@gmail.com <a0975844268@gmail.com>
2021-08-03 15:54:33 +0800

    Signed-off-by: a0975844268@gmail.com <a0975844268@gmail.com>

--- rclone.py
+++ rclone.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import os 
 import subprocess
@@ -7,6 +8,7 @@ def system_call(command):
     return p.stdout.read()
 
 arg1=sys.argv[1]
+
 
 id = ""
 shell = ""
@@ -20,45 +22,55 @@ if arg1 == "-h":
     print("    python3 rclone.py copy url gdrive_name mount_destination")
     print("    EX:")
     print("        python3 rclone.py copy https://drive.google.com/drive/folders/1XXXX t10 /mnt/t10/test")
-
-elif arg1 == "mount":
-    gdrive_name = sys.argv[2]
-    mount_destination = sys.argv[3]
-    shell = "rclone mount \
-        --allow-other \
-        --cache-tmp-upload-path=/tmp/rclone/upload \
-        --cache-chunk-path=/tmp/rclone/chunks \
-        --cache-workers=8 \
-        --cache-writes \
-        --cache-dir=/tmp/rclone/vfs \
-        --cache-db-path=/tmp/rclone/db \
-        --no-modtime \
-        --drive-use-trash \
-        --stats=0 \
-        --checkers=16 \
-        --bwlimit=40M \
-        --dir-cache-time=60m \
-        --vfs-cache-mode full \
-        --cache-info-age=60m "+gdrive_name+": "+mount_destination+" &"
-    #print(shell)
-    print(system_call(shell))
-
-elif arg1 == "copy":
-    url = sys.argv[2]
-    gdrive_name = sys.argv[3]
-    mount_destination = sys.argv[4]
-    url_split=url.split('/')
-
-    if mount_destination[-1] != "/":
-        mount_destination += "/"
-
-    for i in url_split:
-        if len(i) > 10 and i[0] == '1' :
-            id=i.split('?')[0]
-
-    if "folders" in url_split:
-        shell = "rclone copy "+gdrive_name+": --drive-root-folder-id "+id+" "+mount_destination+" -v"
-    if "file" in url_split:
-        shell = "rclone backend copyid "+gdrive_name+": "+id+" "+mount_destination+" -v"
-    #print(shell)
-    print(system_call(shell))
+try:
+    if arg1 == "mount":
+        gdrive_name = sys.argv[2]
+        mount_destination = sys.argv[3]
+        shell = "rclone mount \
+            --allow-other \
+            --cache-tmp-upload-path=/tmp/rclone/upload \
+            --cache-chunk-path=/tmp/rclone/chunks \
+            --cache-workers=8 \
+            --cache-writes \
+            --cache-dir=/tmp/rclone/vfs \
+            --cache-db-path=/tmp/rclone/db \
+            --no-modtime \
+            --drive-use-trash \
+            --stats=0 \
+            --checkers=16 \
+            --bwlimit=40M \
+            --dir-cache-time=60m \
+            --vfs-cache-mode full \
+            --cache-info-age=60m "+gdrive_name+": "+mount_destination+" &"
+        #print(shell)
+        print(system_call(shell))
+except:
+    print("Syntax error!!!!!!")
+    print("python3 rclone.py mount gdrive_name mount_destination")
+try:
+    if arg1 == "copy":
+        url = sys.argv[2]
+        gdrive_name = sys.argv[3]
+        mount_destination = sys.argv[4]
+        url_split=url.split('/')
+    
+        if mount_destination[-1] != "/":
+            mount_destination += "/"
+    
+        if gdrive_name[-1] == ":":
+            gdrive_name=gdrive_name[0:-1]
+    
+        for i in url_split:
+            if len(i) > 10 and i[0] == '1' :
+                id=i.split('?')[0]
+    
+        if "folders" in url_split:
+            shell = "rclone copy "+gdrive_name+": --drive-root-folder-id "+id+" "+mount_destination+" -v"
+        if "file" in url_split:
+            shell = "rclone backend copyid "+gdrive_name+": "+id+" "+mount_destination+" -v"
+        print(shell)
+        
+        #print(system_call(shell))
+except:
+    print("Syntax error!!!!!!")
+    print("python3 rclone.py copy url gdrive_name mount_destination")
